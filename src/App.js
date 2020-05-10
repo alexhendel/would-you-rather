0f41b1a25796b62690d3a196f869e2906bdac0ed
Alexander Hendel <alexander.hendel@gmail.com>
2020-05-10 13:52:00 +0200

    Implement 404 component for unknown urls

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import NoMatch from './components/NoMatch';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          {/* <Home /> */}
+        </Route>
+        <Route exact path="/signin">
+          {/* <SignIn /> */}
+        </Route>
+        <Route exact path="/add">
+          {/* <AddPoll /> */}
+        </Route>
+        <Route exact path="/leaderboard">
+          {/* <LeaderBoard /> */}
+        </Route>
+        <Route>
+          <NoMatch />
+        </Route>
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/components/NoMatch.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
+
+const NoMatch = () => {
+  let location = useLocation();
+  return (
+    <>
+      <h1>404 - Not Found</h1>
+      <p>
+        Sorry the URL <b>{location.pathname}</b> was not found. Get back{' '}
+        <Link to="/">/home</Link>.
+      </p>
+    </>
+  );
+};
+
+export default NoMatch;
