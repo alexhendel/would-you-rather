268a2de973b3a9ce3aaf218c9471c9d9bea6c2c6
Alexander Hendel <alexander.hendel@gmail.com>
2020-05-11 11:13:08 +0200

    Simplify NavigationBar layout

--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -4,12 +4,9 @@ import { makeStyles } from '@material-ui/core/styles';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Button from '@material-ui/core/Button';
-import IconButton from '@material-ui/core/IconButton';
 import Avatar from '@material-ui/core/Avatar';
-import Menu from '@material-ui/core/Menu';
-import MenuItem from '@material-ui/core/MenuItem';
 import Typography from '@material-ui/core/Typography';
-import MoreVertIcon from '@material-ui/icons/MoreVert';
+import Divider from '@material-ui/core/Divider';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -25,62 +22,35 @@ const useStyles = makeStyles((theme) => ({
     width: theme.spacing(4),
     height: theme.spacing(4),
     marginLeft: theme.spacing(1),
-  },
-  more: {
-    color: theme.palette.primary.contrastText,
+    boxShadow: theme.shadows[2],
   },
 }));
 
 const NavigationBar = (props) => {
   const classes = useStyles();
-  const [anchorEl, setAnchorEl] = React.useState(null);
-
-  const handleClick = (event) => {
-    setAnchorEl(event.currentTarget);
-  };
-
-  const handleClose = () => {
-    setAnchorEl(null);
-  };
 
   return (
     <div className={classes.root}>
-      <AppBar position="static">
+      <AppBar position="static" elevation={0} color="transparent">
         <Toolbar>
           <Typography variant="h6" className={classes.title}>
             {props.title}
           </Typography>
           <span className={classes.spacer} />
-          <Button variant="contained" color="primary" disableElevation>
-            Home
-          </Button>
-          <Button variant="contained" color="primary" disableElevation>
-            New Question
-          </Button>
-          <Button variant="contained" color="primary" disableElevation>
-            Leader Board
-          </Button>
-          <span className={classes.spacer} />
 
           <Typography>Hello, user!</Typography>
-          <Avatar className={classes.avatarSmall}>HB</Avatar>
-          <IconButton
-            aria-label="more"
-            className={classes.more}
-            onClick={handleClick}
-          >
-            <MoreVertIcon />
-          </IconButton>
-
-          <Menu
-            id="profile-menu"
-            anchorEl={anchorEl}
-            keepMounted
-            open={Boolean(anchorEl)}
-            onClose={handleClose}
-          >
-            <MenuItem onClick={handleClose}>Sign out</MenuItem>
-          </Menu>
+          <Avatar className={classes.avatarSmall} elevation={4}>
+            HB
+          </Avatar>
+          <Button>Sign out</Button>
+        </Toolbar>
+        <Divider />
+        <Toolbar>
+          <span className={classes.spacer} />
+          <Button disableElevation>Home</Button>
+          <Button disableElevation>New Question</Button>
+          <Button disableElevation>Leader Board</Button>
+          <span className={classes.spacer} />
         </Toolbar>
       </AppBar>
     </div>
