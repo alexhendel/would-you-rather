1cca7eb37b80a9adb43447d3147b7e6dba01a746
Alexander Hendel <alexander.hendel@gmail.com>
2020-05-14 21:14:47 +0200

    Add basic layout for new questions

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,13 +1,31 @@
 import React from 'react';
-// import { useSelector } from 'react-redux';
+import { useSelector } from 'react-redux';
+import * as API from '../api/_DATA';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import Card from '@material-ui/core/Card';
+import CardContent from '@material-ui/core/CardContent';
+import CardActions from '@material-ui/core/CardActions';
 
 const NewQuestion = () => {
-  // const authedUser = useSelector((state) => state.authedUser);
+  const authedUser = useSelector((state) => state.authedUser);
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log('submit for ', authedUser ?? authedUser.id);
+  };
 
   return (
-    <>
-      <p>New</p>
-    </>
+    <Box display="flex" alignItems="center" flexDirection="column">
+      <Card variant="outlined">
+        <CardContent></CardContent>
+        <CardActions>
+          <Button variant="contained" color="primary" onClick={handleSubmit}>
+            Submit
+          </Button>
+        </CardActions>
+      </Card>
+    </Box>
   );
 };
 
