0fef8c13454e6137f0b3ddab60cea18f206e9384
Alexander Hendel <alexander.hendel@gmail.com>
2020-05-15 09:18:05 +0200

    Add UI for handling new questions

--- src/components/NewQuestion.js
+++ src/components/NewQuestion.js
@@ -1,26 +1,100 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import * as API from '../api/_DATA';
+import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import CardContent from '@material-ui/core/CardContent';
 import CardActions from '@material-ui/core/CardActions';
+import Typography from '@material-ui/core/Typography';
+import TextField from '@material-ui/core/TextField';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    width: '550px',
+  },
+  fullWidth: {
+    width: '100%',
+  },
+  formRoot: {
+    marginTop: theme.spacing(8),
+  },
+}));
 
 const NewQuestion = () => {
   const authedUser = useSelector((state) => state.authedUser);
+  const classes = useStyles();
+  const [optionOne, setOptionOne] = React.useState('');
+  const [optionTwo, setOptionTwo] = React.useState('');
 
+  const updateOptionOneValue = (event) => {
+    setOptionOne(event.target.value);
+  };
+  const updateOptionTwoValue = (event) => {
+    setOptionTwo(event.target.value);
+  };
   const handleSubmit = (event) => {
     event.preventDefault();
-    console.log('submit for ', authedUser ?? authedUser.id);
+
+    const newQuestion = {
+      optionOneText: optionOne,
+      optionTwoText: optionTwo,
+      author: authedUser,
+    };
+
+    console.log('submit question: ', newQuestion);
   };
 
   return (
     <Box display="flex" alignItems="center" flexDirection="column">
-      <Card variant="outlined">
-        <CardContent></CardContent>
+      <Card variant="outlined" className={classes.root}>
+        <CardContent>
+          <Typography variant="h5" component="h2">
+            Create New Question
+          </Typography>
+          <Typography variant="body2" component="p">
+            Complete the question:
+          </Typography>
+          <Typography className={classes.formRoot} variant="h6">
+            <Box display="flex" alignItems="center" flexDirection="column">
+              <form className={classes.fullWidth} autoComplete="off">
+                <span className={classes.fullWidth}>Would you rather...</span>
+                <TextField
+                  className={classes.fullWidth}
+                  required
+                  id="option-one"
+                  label="Required"
+                  value={optionOne}
+                  onChange={updateOptionOneValue}
+                  placeholder="Enter option one here."
+                />
+                <div>
+                  <span className={classes.fullWidth}></span>
+                  <span>- OR -</span>
+                  <span className={classes.fullWidth}></span>
+                </div>
+                <TextField
+                  className={classes.fullWidth}
+                  required
+                  id="option-two"
+                  label="Required"
+                  value={optionTwo}
+                  onChange={updateOptionTwoValue}
+                  placeholder="Enter option two here."
+                />
+              </form>
+            </Box>
+          </Typography>
+        </CardContent>
         <CardActions>
-          <Button variant="contained" color="primary" onClick={handleSubmit}>
+          <Button
+            className={classes.fullWidth}
+            variant="contained"
+            color="primary"
+            onClick={handleSubmit}
+            disabled={!optionOne || !optionTwo}
+          >
             Submit
           </Button>
         </CardActions>
