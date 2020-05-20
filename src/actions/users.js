dbce238a6e65d1cb5ac97488055280e9d900b91a
Alexander Hendel <alexander.hendel@gmail.com>
2020-05-20 22:40:55 +0200

    Add users actions and reducers

--- src/actions/users.js
+++ src/actions/users.js
@@ -1,1 +1,16 @@
-// tbd
+import * as API from '../api/_DATA';
+
+export const RECEIVE_USERS = 'RECEIVE_USERS';
+
+function receiveUsers(users) {
+  return {
+    type: RECEIVE_USERS,
+    users,
+  };
+}
+
+export function handleReceiveUsers() {
+  return (dispatch) => {
+    return API._getUsers().then((users) => dispatch(receiveUsers(users)));
+  };
+}
--- src/reducers/users.js
+++ src/reducers/users.js
@@ -1,7 +1,10 @@
 import { RECEIVE_DATA } from '../actions/shared';
+import { RECEIVE_USERS } from '../actions/users';
 
 export default function users(state = [], action) {
   switch (action.type) {
+    case RECEIVE_USERS:
+      return action.users;
     case RECEIVE_DATA:
       return action.users;
     default:
