91bf0affb05f8c82fc0458b6d77ae55870138135
Alexander Hendel <alexander.hendel@gmail.com>
2020-05-20 22:40:33 +0200

    Fix file extension for import

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,7 +3,7 @@ import { combineReducers } from 'redux';
 import users from './users';
 import questions from './questions';
 import loading from './loading';
-import authedUser from './authedUser.js';
+import authedUser from './authedUser';
 
 export default combineReducers({
   users,
