cf0f958bb498f35cb5d1f697c024bba8e33ed9ff
Alexander Hendel <alexander.hendel@gmail.com>
2020-05-21 20:34:17 +0200

    Refactor vote component

--- src/components/QuestionVote.js
+++ src/components/QuestionVote.js
@@ -1,8 +1,7 @@
-import React from 'react';
+import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import { useSelector } from 'react-redux';
-import { useDispatch } from 'react-redux';
-import { useHistory } from 'react-router-dom';
+import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 import { handleSaveQuestionAnswer } from '../actions/questions';
 import Grid from '@material-ui/core/Grid';
 import Typography from '@material-ui/core/Typography';
@@ -12,79 +11,97 @@ import Radio from '@material-ui/core/Radio';
 import RadioGroup from '@material-ui/core/RadioGroup';
 import FormControlLabel from '@material-ui/core/FormControlLabel';
 import FormControl from '@material-ui/core/FormControl';
-import { makeStyles } from '@material-ui/styles';
+import { withStyles } from '@material-ui/core/styles';
 
-const useStyles = makeStyles((theme) => ({
+const styles = (theme) => ({
   root: {
-    padding: '1em',
+    padding: theme.spacing(2),
   },
   fullWidth: {
     width: '100%',
   },
-}));
+});
 
-const QuestionVote = (props) => {
-  const authedUser = useSelector((state) => state.authedUser);
-  const loading = useSelector((state) => state.loading);
-  const history = useHistory();
-  const classes = useStyles();
-  const dispatch = useDispatch();
-  const [choice, setChoice] = React.useState('optionOne');
-
-  const handleChange = (event) => {
-    setChoice(event.target.value);
+class QuestionVote extends Component {
+  state = {
+    choice: 'optionOne',
   };
-
-  const handleSubmit = (event) => {
+  handleChange = (event) => {
+    const choice = event.target.value;
+    this.setState(() => ({ choice }));
+  };
+  handleSubmit = (event) => {
     event.preventDefault();
+    const { dispatch, authedUser, history } = this.props;
+
     dispatch(
-      handleSaveQuestionAnswer(authedUser, props.question.id, choice)
+      handleSaveQuestionAnswer(
+        authedUser,
+        this.props.question.id,
+        this.state.choice
+      )
     ).then(() => {
       history.push('/');
-      history.push(`/questions/${props.question.id}`);
+      history.push(`/questions/${this.props.question.id}`);
     });
   };
-
-  return (
-    <Paper className={classes.root} variant="outlined">
-      <Grid container spacing={1}>
-        <Grid item xs={12}>
-          <Typography variant="h6">Would you rather...</Typography>
+  render() {
+    const { classes } = this.props;
+    const { loading } = this.props;
+    return (
+      <Paper className={classes.root} variant="outlined">
+        <Grid container spacing={1}>
+          <Grid item xs={12}>
+            <Typography variant="h6">Would you rather...</Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <FormControl component="fieldset">
+              <RadioGroup
+                name="options"
+                value={this.state.choice}
+                onChange={this.handleChange}
+              >
+                <FormControlLabel
+                  value="optionOne"
+                  control={<Radio />}
+                  label={this.props.question.optionOne.text}
+                />
+                <FormControlLabel
+                  value="optionTwo"
+                  control={<Radio />}
+                  label={this.props.question.optionTwo.text}
+                />
+              </RadioGroup>
+            </FormControl>
+          </Grid>
+          <Grid item xs={12}>
+            <Button
+              color="primary"
+              className={classes.fullWidth}
+              variant="contained"
+              disabled={loading}
+              onClick={this.handleSubmit}
+            >
+              Submit
+            </Button>
+          </Grid>
         </Grid>
-        <Grid item xs={12}>
-          <FormControl component="fieldset">
-            <RadioGroup name="options" value={choice} onChange={handleChange}>
-              <FormControlLabel
-                value="optionOne"
-                control={<Radio />}
-                label={props.question.optionOne.text}
-              />
-              <FormControlLabel
-                value="optionTwo"
-                control={<Radio />}
-                label={props.question.optionTwo.text}
-              />
-            </RadioGroup>
-          </FormControl>
-        </Grid>
-        <Grid item xs={12}>
-          <Button
-            color="primary"
-            className={classes.fullWidth}
-            variant="contained"
-            disabled={loading}
-            onClick={handleSubmit}
-          >
-            Submit
-          </Button>
-        </Grid>
-      </Grid>
-    </Paper>
-  );
-};
+      </Paper>
+    );
+  }
+}
 
 QuestionVote.propTypes = {
   question: PropTypes.object.isRequired,
 };
 
-export default QuestionVote;
+function mapStateToProps({ authedUser, loading }) {
+  return {
+    authedUser,
+    loading,
+  };
+}
+
+export default connect(mapStateToProps)(
+  withRouter(withStyles(styles, { withTheme: true })(QuestionVote))
+);
