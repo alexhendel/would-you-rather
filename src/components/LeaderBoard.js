4b6afcc93a9d2df9927ce2c4ed127fc3d477d957
Alexander Hendel <alexander.hendel@gmail.com>
2020-05-22 12:50:23 +0200

    Refactor LeaderBoard component

--- src/components/LeaderBoard.js
+++ src/components/LeaderBoard.js
@@ -1,32 +1,35 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import Box from '@material-ui/core/Box';
 import LeaderBoardItem from './LeaderBoardItem';
 
-const LeaderBoard = () => {
-  const users = useSelector((state) => state.users);
-  // user object is not an array but a object -
-  // this code will iterate through the
-  // object keys and add the users to a user array
-  let userArray = [];
-  Object.keys(users).forEach(function (key) {
-    userArray.push({
-      ...users[key],
-      numberOfQuestion: users[key].questions.length,
-      numberOfAnswers: Object.keys(users[key].answers).length,
-      score:
-        users[key].questions.length + Object.keys(users[key].answers).length,
+class LeaderBoard extends Component {
+  userList = () => {
+    const { users } = this.props;
+    let list = [];
+    Object.keys(users).forEach(function (key) {
+      list.push({
+        ...users[key],
+        numberOfQuestion: users[key].questions.length,
+        numberOfAnswers: Object.keys(users[key].answers).length,
+        score:
+          users[key].questions.length + Object.keys(users[key].answers).length,
+      });
     });
-  });
-  userArray.sort((elementA, elementB) => elementB.score - elementA.score);
+    list.sort((elementA, elementB) => elementB.score - elementA.score);
+    return list;
+  };
+  render() {
+    return (
+      <Box display="flex" alignItems="center" flexDirection="column">
+        {this.userList().map((user) => {
+          return <LeaderBoardItem key={user.id} user={user} />;
+        })}
+      </Box>
+    );
+  }
+}
 
-  return (
-    <Box display="flex" alignItems="center" flexDirection="column">
-      {userArray.map((user) => {
-        return <LeaderBoardItem key={user.id} user={user} />;
-      })}
-    </Box>
-  );
-};
-
-export default LeaderBoard;
+export default connect((state) => ({
+  users: state.users,
+}))(LeaderBoard);
