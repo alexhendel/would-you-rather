3c65336e4b81deb7e218025bbca3c67dad368128
Alexander Hendel <alexander.hendel@gmail.com>
2020-05-22 13:52:27 +0200

    Refactor QuestionListItem component

--- src/components/QuestionListItem.js
+++ src/components/QuestionListItem.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
 import UserAvatar from './UserAvatar';
-import { makeStyles } from '@material-ui/styles';
+import { withStyles } from '@material-ui/styles';
 import Paper from '@material-ui/core/Paper';
 import Grid from '@material-ui/core/Grid';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
 import { whoAsked } from '../utils/utils';
 
-const useStyles = makeStyles((theme) => ({
+const styles = (theme) => ({
   root: {
     marginTop: '1em',
     padding: '1em',
@@ -19,55 +19,58 @@ const useStyles = makeStyles((theme) => ({
   fullWidth: {
     width: '100%',
   },
-}));
+});
 
-const QuestionListItem = (props) => {
-  const users = useSelector((state) => state.users);
-  const classes = useStyles();
-
-  return (
-    <>
-      <Paper className={classes.root} variant="outlined">
-        <Grid container spacing={3}>
-          <Grid item xs={12}>
-            <Typography variant="h5">
-              {whoAsked(users, props.question).name} asks...
-            </Typography>
-          </Grid>
-          <Grid item xs={2}>
-            <Grid container justify="center" alignItems="center">
-              <UserAvatar user={whoAsked(users, props.question)} />
+class QuestionListItem extends Component {
+  render() {
+    const { users } = this.props;
+    const { classes } = this.props;
+    return (
+      <>
+        <Paper className={classes.root} variant="outlined">
+          <Grid container spacing={3}>
+            <Grid item xs={12}>
+              <Typography variant="h5">
+                {whoAsked(users, this.props.question).name} asks...
+              </Typography>
             </Grid>
-          </Grid>
-          <Grid item xs={10}>
-            <Grid container spacing={1}>
-              <Grid item xs={12}>
-                <Typography variant="h6">Would you rather...</Typography>
-                <Typography variant="body2">
-                  {props.question.optionOne.text}...
-                </Typography>
-              </Grid>
-              <Grid item xs={12}>
-                <Button
-                  className={classes.fullWidth}
-                  variant="contained"
-                  color="primary"
-                  component={Link}
-                  to={`/questions/${props.question.id}`}
-                >
-                  View Poll
-                </Button>
+            <Grid item xs={2}>
+              <Grid container justify="center" alignItems="center">
+                <UserAvatar user={whoAsked(users, this.props.question)} />
               </Grid>
             </Grid>
+            <Grid item xs={10}>
+              <Grid container spacing={1}>
+                <Grid item xs={12}>
+                  <Typography variant="h6">Would you rather...</Typography>
+                  <Typography variant="body2">
+                    {this.props.question.optionOne.text}...
+                  </Typography>
+                </Grid>
+                <Grid item xs={12}>
+                  <Button
+                    className={classes.fullWidth}
+                    variant="contained"
+                    color="primary"
+                    component={Link}
+                    to={`/questions/${this.props.question.id}`}
+                  >
+                    View Poll
+                  </Button>
+                </Grid>
+              </Grid>
+            </Grid>
           </Grid>
-        </Grid>
-      </Paper>
-    </>
-  );
-};
+        </Paper>
+      </>
+    );
+  }
+}
 
 QuestionListItem.propTypes = {
   question: PropTypes.object.isRequired,
 };
 
-export default QuestionListItem;
+export default connect((state) => ({
+  users: state.users,
+}))(withStyles(styles, { withTheme: true })(QuestionListItem));
